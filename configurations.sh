a9e23cdd96619e21f8bd51ab5ebdc39da841f86d
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-03-23 13:14:47 +0300

    Update configurations.sh

--- configurations.sh
+++ configurations.sh
@@ -7,6 +7,7 @@ mkdir -p ~/.config/pipewire/pipewire.conf.d && wget https://raw.githubuserconten
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/10-sound.conf && mv 10-sound.conf ~/.config/pipewire/pipewire.conf.d/
 mkdir ~/.config/fish && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/config.fish && mv config.fish ~/.config/fish/
 mkdir ~/.config/fish/functions/ && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/fish_prompt.fish && mv fish_prompt.fish ~/.config/fish/functions/
+wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/nvidia-kms.conf && sudo mv nvidia-kms.conf /etc/modprobe.d/
 #wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/nvidia-kms.conf && sudo mv nvidia-kms.conf /etc/modprobe.d/
 #mkdir ~/.steam/ ~/.steam/steam/ && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/steam_dev.cfg && mv steam_dev.cfg ~/.steam/steam/
 #mkdir ~/.config/OpenRGB && wget https://github.com/Bunnysword/Confiles/raw/refs/heads/main/conf/1.orp && mv 1.orp ~/.config/OpenRGB/
