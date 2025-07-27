fcb8808dadb651df96e1a20e5d5baa6ccef29455
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-07-27 21:25:36 +0300

    Delete zapretcon/zapret.sh

--- zapretcon/zapret.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux && cd zapret-discord-youtube-linux/
-git clone https://github.com/Flowseal/zapret-discord-youtube zapret-latest
-rm service.sh && rm conf.env
-wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/zapretcon/conf.env
-wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/zapretcon/service.sh
-chmod 750 service.sh && chmod 750 nfqws && chmod 750 main_script.sh && chmod 750 stop_and_clean_nft.sh
-mv 'zapret-latest/general (ALT4).bat' zapret-latest/gen.bat
-./service.sh
