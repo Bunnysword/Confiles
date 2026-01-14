ade30008cb27b201c55b0f709b5a1e8f6b2f4dc0
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2026-01-14 06:31:40 +0300

    Update configurations.sh

--- configurations.sh
+++ configurations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/.config/{MangoHud,pipewire/pipewire.conf.d,fish/functions}
+mkdir -p ~/.config/{MangoHud,fastfetch,pipewire/pipewire.conf.d,fish/functions}
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/urlconf.txt && wget -i urlconf.txt
 mv MangoHud.conf ~/.config/MangoHud/
 mv 10-no-resampling.conf ~/.config/pipewire/pipewire.conf.d/
@@ -9,6 +9,17 @@ mv config.fish ~/.config/fish/
 mv fish_prompt.fish ~/.config/fish/functions/
 mv -i plasmashellrc ~/.config
 mv -i breezerc ~/.config
+
+mkdir ~/.local/share/color-schemes
+mv SwordColors.colors ~/.local/share/color-schemes/
+mv swordprofile.profile ~/.local/share/konsole/
+mv WhiteOnBlack.colorscheme ~/.local/share/konsole/
+mv kcm-about-distrorc ~/.config/kdedefaults/
+cp bugs.png ~/.config/kdedefaults/
+mv bugs.png ~/.config/fastfetch/
+mv config.jsonc ~/.config/fastfetch/
+
+plasma-apply-colorscheme SwordColors
 sudo mv nvidia-kms.conf /etc/modprobe.d/
 sudo mv environment /etc
 systemctl --user restart plasma-plasmashell
