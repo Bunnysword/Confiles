0ea8aec3a083efa2b4857029414990b5320c752b
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2026-01-14 04:01:49 +0300

    Update configurations.sh

--- configurations.sh
+++ configurations.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-
-#configs
-mkdir ~/.config/MangoHud && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/MangoHud.conf && mv MangoHud.conf ~/.config/MangoHud/
-wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/environment && sudo mv environment /etc
-mkdir -p ~/.config/pipewire/pipewire.conf.d && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/10-no-resampling.conf && mv 10-no-resampling.conf ~/.config/pipewire/pipewire.conf.d/
-wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/10-sound.conf && mv 10-sound.conf ~/.config/pipewire/pipewire.conf.d/
-mkdir ~/.config/fish && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/config.fish && mv config.fish ~/.config/fish/
-mkdir ~/.config/fish/functions/ && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/fish_prompt.fish && mv fish_prompt.fish ~/.config/fish/functions/
-wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/nvidia-kms.conf && sudo mv nvidia-kms.conf /etc/modprobe.d/
-#mkdir ~/.steam/ ~/.steam/steam/ && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/steam_dev.cfg && mv steam_dev.cfg ~/.steam/steam/
-
-#Autostart
-#mkdir ~/.config/autostart
-#wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/autostart/org.mozilla.firefox.desktop
-#wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/autostart/org.telegram.desktop.desktop
-#wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/autostart/steam.desktop
-#mv *.desktop ~/.config/autostart
-
-#Wakeup for Aorus Elite v2 fix 
-#wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/services/wakeup-disable_GPP0.service
-#sudo cp wakeup-disable_GPP0.service /etc/systemd/system/ && sudo systemctl enable wakeup-disable_GPP0.service
-#sudo systemctl start wakeup-disable_GPP0.service
+mkdir -p ~/.config/{MangoHud,pipewire/pipewire.conf.d,fish/functions}
+wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/urlconf.txt && wget -i urlconf.txt
+mv MangoHud.conf ~/.config/MangoHud/
+mv 10-no-resampling.conf ~/.config/pipewire/pipewire.conf.d/
+mv 10-sound.conf ~/.config/pipewire/pipewire.conf.d/
+mv config.fish ~/.config/fish/
+mv fish_prompt.fish ~/.config/fish/functions/
+sudo mv nvidia-kms.conf /etc/modprobe.d/
+sudo mv environment /etc
